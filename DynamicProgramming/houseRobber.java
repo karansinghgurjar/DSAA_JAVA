a2a17db47e6bf6ce44d09b95455b513b3c92dac2
Karan Singh Gurjar <karansinghgurjar@users.noreply.github.com>
2026-05-18 17:08:38 +0530

    Add house robber dynamic programming solutions

---
+++ DynamicProgramming/houseRobber.java
@@ -0,0 +1,47 @@
+package DynamicProgramming;
+import java.util.Arrays;
+
+
+public class houseRobber {
+    public int recursion(int[] nums , int index){
+        if(index >= nums.length) return 0; //base condition
+
+        int rob = nums[index] + recursion(nums, index + 2);
+        int skip = recursion(nums, index + 1);
+
+        return Math.max(rob , skip);
+    }
+    public int memoization(int[] nums , int index , int[] dp){
+        if(index >= nums.length) return 0;
+        if(dp[index] != -1) return dp[index];
+
+        int rob = nums[index] + memoization(nums, index + 2, dp);
+        int skip = memoization(nums, index + 1, dp);
+
+        dp[index] = Math.max(rob,skip);
+        return dp[index];
+    }
+    public int tabulation(int[] nums){
+        if(nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        for(int index = 1;index < nums.length;index++){
+            int take = nums[index];
+            if(index > 1) take+=dp[index - 2];
+
+            int notTake = dp[index - 1];
+            dp[index] = Math.max(take,notTake);
+        }
+        return dp[nums.length - 1];
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,9,3,1};
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        houseRobber obj = new houseRobber();
+        System.out.println(obj.recursion(nums, 0));
+        System.out.println(obj.memoization(nums, 0,dp));
+    }
+    
+}
